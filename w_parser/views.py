1c1ae3a6e943bbc442b823a20b1dc7acb69b134d
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-07-04 17:47:07 +0400

    chore: update CSS

--- w_parser/views.py
+++ w_parser/views.py
@@ -10,23 +10,25 @@ def custom_404(request, exception):
 
 
 def index(request):
-    query = request.GET.get("query", "")
+    query = request.GET.get("query", "").strip()
+    products = []
 
-    products = [
-        {
-            "name": "Пример товара 1",
-            "price": 5000,
-            "discount_price": 4000,
-            "rating": 4.5,
-            "reviews": 120,
-        },
-        {
-            "name": "Пример товара 2",
-            "price": 3200,
-            "discount_price": 2900,
-            "rating": 4.0,
-            "reviews": 75,
-        },
-    ]
+    if query:
+        products = [
+            {
+                "name": f"Товар «{query}» пример 1",
+                "price": 5000,
+                "discount_price": 4000,
+                "rating": 4.5,
+                "reviews": 120,
+            },
+            {
+                "name": f"Товар «{query}» пример 2",
+                "price": 3200,
+                "discount_price": 2900,
+                "rating": 4.0,
+                "reviews": 75,
+            },
+        ]
 
     return render(request, "index.html", {"query": query, "products": products})
